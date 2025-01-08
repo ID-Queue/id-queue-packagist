71bc3bfca7694c4228274a1973f7fd28dbcd89df
deepakdumraliya1998 <deepakdumraliya1998@gmail.com>
2025-01-08 20:34:12 +0530

    IDQ-176: Added where condition for stationed false

--- src/Models/Company/User.php
+++ src/Models/Company/User.php
@@ -183,7 +183,7 @@ class User extends Authenticatable
     public static function getUsersByStatus(UserStatus $status): Collection|_IH_User_C|array
     {
         // Get all users that could match the status.
-        $users = self::all();
+        $users = self::where('isStationed', false)->get();
 
         if ($status->value === UserStatus::Stationed()->value) {
             return self::where(['isStationed' => true, 'Staff_Login_State' => 1])->get();
