f43bff23c3f55d3b15395ba56afaf0c1743de7da
deepakdums <deepakdumraliya1998@gmail,com>
2025-02-19 17:13:29 +0530

    Added StaffStatus Service

---
+++ src/Services/StaffStatusLogService.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace IdQueue\IdQueuePackagist\Services;
+
+use IdQueue\IdQueuePackagist\Models\Company\StatusLog;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
+
+class StaffStatusLogService
+{
+    /**
+     * Log a staff status event.
+     *
+     * @param int $companyDeptId
+     * @param int $staffStatus
+     * @param string $message
+     * @return void
+     */
+    public function logStaffStatusEvent(int $companyDeptId, int $staffStatus, string $message): void
+    {
+        try {
+            StatusLog::create([
+                'Staff_GUID' => Auth::user()->GUID,
+                'Company_Dept_ID' => $companyDeptId,
+                'Staff_Status' => $staffStatus,
+                'Staff_Detail' => $message,
+                'Staff_Action_DateTime' => Carbon::now(),
+            ]);
+        } catch (\Exception $e) {
+            Log::error('Failed to log staff status event: ' . $e->getMessage());
+        }
+    }
+}
